bbd9fd03a2afe1950116c59bce52cfc1568ca973
treylewis <trey.lewis@yale.edu>
2022-12-19 22:01:05 -0500

    Final edits reversi

--- reversi.py
+++ reversi.py
@@ -190,3 +190,124 @@ def getBoardCopy(board):
 def isOnCorner(x, y):
     # Returns True if the position is in one of the four corners.
     return (x == 0 and y == 0) or (x == 7 and y == 0) or (x == 0 and y == 7) or (x == 7 and y == 7)
+
+
+
+
+def getPlayerMove(board, playerTile):
+    # Let the player type in their move.
+    # Returns the move as [x, y] (or returns the strings 'hints' or 'quit')
+    DIGITS1TO8 = '1 2 3 4 5 6 7 8'.split()
+    while True:
+        print('Enter your move, or type quit to end the game, or hints to turn off/on hints.')
+        move = input().lower()
+        if move == 'quit':
+            return 'quit'
+        if move == 'hints':
+            return 'hints'
+
+        if len(move) == 2 and move[0] in DIGITS1TO8 and move[1] in DIGITS1TO8:
+            x = int(move[0]) - 1
+            y = int(move[1]) - 1
+            if isValidMove(board, playerTile, x, y) == False:
+                continue
+            else:
+                break
+        else:
+            print('That is not a valid move. Type the x digit (1-8), then the y digit (1-8).')
+            print('For example, 81 will be the top-right corner.')
+    return [x, y]
+
+
+
+def getComputerMove(board, computerTile):
+    # Given a board and the computer's tile, determine where to
+    # move and return that move as a [x, y] list.
+    possibleMoves = getValidMoves(board, computerTile)
+    # randomize the order of the possible moves
+    random.shuffle(possibleMoves)
+
+    # always go for a corner if available.
+    for x, y in possibleMoves:
+        if isOnCorner(x, y):
+            return [x, y]
+    # Go through all the possible moves and remember the best scoring move
+    bestScore = -1
+    for x, y in possibleMoves:
+        dupeBoard = getBoardCopy(board)
+        makeMove(dupeBoard, computerTile, x, y)
+        score = getScoreOfBoard(dupeBoard)[computerTile]
+        if score > bestScore:
+            bestMove = [x, y]
+            bestScore = score
+    return bestMove
+
+def showPoints(playerTile, computerTile):
+    # Prints out the current score.
+    scores = getScoreOfBoard(mainBoard)
+    print('You have %s points. The computer has %s points.' % (scores[playerTile], scores[computerTile]))
+
+
+
+
+print('Welcome to Reversi!')
+
+
+while True:
+    # Reset the board and game.
+    mainBoard = getNewBoard()
+    resetBoard(mainBoard)
+    playerTile, computerTile = enterPlayerTile()
+    showHints = False
+    turn = whoGoesFirst()
+    print('The ' + turn + ' will go first.')
+    while True:
+        if turn == 'player':
+            # Player's turn.
+            if showHints:
+                validMovesBoard = getBoardWithValidMoves(mainBoard, playerTile)
+                drawBoard(validMovesBoard)
+            else:
+                drawBoard(mainBoard)
+            showPoints(playerTile, computerTile)
+            move = getPlayerMove(mainBoard, playerTile)
+            if move == 'quit':
+                print('Thanks for playing!')
+                sys.exit() # terminate the program
+            elif move == 'hints':
+                showHints = not showHints
+                continue
+            else:
+                makeMove(mainBoard, playerTile, move[0], move[1])
+
+
+            if getValidMoves(mainBoard, computerTile) == []:
+                break
+            else:
+                turn = 'computer'
+
+        else:
+            # Computer's turn
+            drawBoard(mainBoard)
+            showPoints(playerTile, computerTile)
+            input('Press Enter to see the computer\'s move.')
+            x, y = getComputerMove(mainBoard, computerTile)
+            makeMove(mainBoard, computerTile, x, y)
+
+            if getValidMoves(mainBoard, playerTile) == []:
+                break
+            else:
+                turn = 'player'
+    # Display the final score.
+    drawBoard(mainBoard)
+    scores = getScoreOfBoard(mainBoard)
+    print('X scored %s points. O scored %s points.' % (scores['X'], scores['O']))
+    if scores[playerTile] > scores[computerTile]:
+        print('You beat the computer by %s points! Congratulations!' % (scores[playerTile] - scores[computerTile]))
+    elif scores[playerTile] < scores[computerTile]:
+        print('You lost. The computer beat you by %s points.' % (scores[computerTile] - scores[playerTile]))
+    else:
+        print('The game was a tie!')
+
+    if not playAgain():
+        break
